639ebba02fe750626efeb4b7a9854b0dc5f949ab
mohdsaadshaikh <mohammadsaad925s4s@gmail.com>
2024-09-29 21:50:34 +0500

    Backend of blog-app-mern repo

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -17,7 +17,6 @@ export const cookieOptions = {
   expires: new Date(
     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
   ),
-  // httpOnly: true,
 };
 
 // export const getBase64 = (file) =>
